8adecbf187005f772f0498cd2404ed0c5894ddc7
Jonathan <poisson.jonathan777@gmail.com>
2016-08-02 22:06:49 -0400

    Initial commit, halfway done with seed. Working on build process now

---
+++ assets/app/boot.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../typings.d.ts" />
+import { boostrap } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
