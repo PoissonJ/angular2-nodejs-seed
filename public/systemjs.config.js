f8dbf355b15b131e033b606fc3c401d4f70d1eb3
Jonathan Poisson <poisson.jonathan777@gmail.com>
2016-08-03 15:45:10 -0400

    Created systemjs file

---
+++ public/systemjs.config.js
@@ -0,0 +1,32 @@
+var map = { // Only need paths that we use in the app and are not imported in html
+  'app': 'js/app',
+  'rxjs': 'js/vendor/rxjs',
+  '@angular': 'js/vendor/@angular'
+};
+var packages = {
+  'app': { main: 'boot.js', defaultExtension: 'js' }, // Append .js on import statements
+  'rxjs': { defaultExtension: 'js' } // Only default Extension, no boot file
+};
+
+var packageNames - [
+  '@angular/common',
+  '@angular/compiler',
+  '@angular/core',
+  '@angular/forms',
+  '@angular/http',
+  '@angular/platform-browser',
+  '@angular/platform-browser-dynamic',
+  '@angular/router',
+  '@angular/upgrade'
+];
+
+packageNames.forEach(function(pkgName) {
+  packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+});
+
+var config = {
+  map: map,
+  packages: packages
+};
+
+System.config(config);
